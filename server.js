5b0623ba9c7d82ee2e7d6514727f87e78ecd21e4
Abichal Shukla <abichalshukla9963@gmail.com>
2025-09-30 17:05:31 +0530

    server

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
- const authRoutes = require('./routes/authRoutes');
-  const bookRoutes = require('./routes/bookRoutes');    
-  const requestRoutes = require('./routes/requestRoute');
-   const path = require('path');
+const authRoutes = require('./routes/authRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+const requestRoutes = require('./routes/requestRoute');
 
+dotenv.config();
 const app = express();
 
 connectDB();
+
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "dist")));
-
-// Catch all unmatched routes and send index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.use('/api', authRoutes);
- app.use('/api', bookRoutes);
- app.use('/api', requestRoutes);
+app.use('/api', bookRoutes);
+app.use('/api', requestRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
